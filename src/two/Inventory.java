1be5cd9920761c5c1b1d89487dafa4fab9ea6ea6
Kapil Dahal <dahalkapildev@gmail.com>
2020-11-28 10:08:23 -0500

    changed some file

---
+++ src/two/Inventory.java
@@ -0,0 +1,10 @@
+package two;
+
+public class Inventory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				//hello checking
+	}
+
+}
---
+++ src/two/Item.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package two;
+
+/**
+ * @author kapilfreeman
+ *
+ */
+public class Item {
+	
+	String name;
+	double price;
+	int quantity;
+	
+	public Item(String name, double price, int quantity) {
+    this.name=name;
+    		this.price=price;
+    		this.quantity=quantity;
+	}
+	public String getName( ) {
+		return name;
+	}
+	public double getPrice( ) {
+		return price;
+	}
+	public int getQuantity( ) {
+		return quantity;
+	}
+	public double getValue( ) {
+		return price*quantity;
+	}
+	
+
+
+}
